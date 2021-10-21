43aae5b99e1658a2fed87c6da25f14e4bf9666b4
coherent17 <mnb51817@gmail.com>
2021-10-21 21:17:55 +0800

    src

--- Matrix Test/generate_matrix.c
+++ Matrix Test/generate_matrix.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 int main(int argc, char *argv[]){
-    int row = atoi(*(argv + 1));
-    int col = atoi(*(argv + 2));
+    int A_row = atoi(*(argv + 1));
+    int A_col = atoi(*(argv + 2));
+    int B_row = atoi(*(argv + 3));
+    int B_col = atoi(*(argv + 4));
     FILE *outputfile;
     outputfile = fopen("matrix", "w");
 
-    //start output
-    for (int i = 0; i < row;i++){
-        for (int j = 0; j < col;j++){
+    //start output matrix A
+    for (int i = 0; i < A_row;i++){
+        for (int j = 0; j < A_col;j++){
             fprintf(outputfile, "%2d ", rand()%100);
         }
         fprintf(outputfile, "\n");
     }
     fprintf(outputfile, "\n");
+
+    //start output matrix B
+    for (int i = 0; i < B_row;i++){
+        for (int j = 0; j < B_col;j++){
+            fprintf(outputfile, "%2d ", rand()%100);
+        }
+        fprintf(outputfile, "\n");
+    }
+
+    //close the file
     fclose(outputfile);
     return 0;
 }
