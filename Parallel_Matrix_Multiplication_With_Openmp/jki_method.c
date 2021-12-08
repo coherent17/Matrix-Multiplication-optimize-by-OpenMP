cab573cb182a733611af56e8e2d52506e35917a6
coherent17 <mnb51817@gmail.com>
2021-12-08 16:18:07 +0800

    jki method

---
+++ Parallel_Matrix_Multiplication_With_Openmp/jki_method.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int A_row;
+int A_col;
+int B_row;
+int B_col;
+
+int **constructMatrix(int row, int col){
+    int **matrix = (int **)malloc(sizeof(int *) * row);
+    for (int i = 0; i < row;i++){
+        matrix[i] = (int *)malloc(sizeof(int) * col);
+    }
+    return matrix;
+}
+
+void freeMatrix(int **matrix, int row, int col){
+    for (int i = 0; i < row;i++){
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+int main(int argc, char *argv[]){
+
+    A_row = atoi(*(argv + 1));
+    A_col = atoi(*(argv + 2));
+    B_row = atoi(*(argv + 3));
+    B_col = atoi(*(argv + 4));
+
+    FILE *input = fopen("matrix", "r");
+
+    int **A = constructMatrix(A_row, A_col);
+    int **B = constructMatrix(B_row, B_col);
+    int **C = constructMatrix(A_row, B_col);
+
+    //read A
+    for (int i = 0; i < A_row;i++){
+        for (int j = 0; j < A_col;j++){
+            fscanf(input, "%d", &A[i][j]);
+        }
+    }
+
+    //read B
+    for (int i = 0; i < B_row;i++){
+        for (int j = 0; j < B_col;j++){
+            fscanf(input, "%d", &B[i][j]);
+        }
+    }
+
+    fclose(input);
+
+    double start_time = omp_get_wtime();
+    //multiply:
+    for (int j = 0; j < B_col;j++){
+        for (int k = 0; k < A_col;k++){
+            int temp = B[k][j];
+            for (int i = 0; i < A_row;i++){
+                C[i][j] += A[i][k] * temp;
+            }
+        }
+    }
+    double end_time = omp_get_wtime();
+    printf("%s: %g sec.\n", "jki_method_runtime", end_time - start_time);
+
+    //output the result to compare with golden result
+    FILE *out = fopen("jki_result", "w");
+    for (int i = 0; i < A_row;i++){
+        for (int j = 0; j < B_col;j++){
+            fprintf(out, "%d ", C[i][j]);
+        }
+        fprintf(out, "\n");
+    }
+    fprintf(out, "\n");
+    fclose(out);
+
+    freeMatrix(A, A_row, A_col);
+    freeMatrix(B, B_row, B_col);
+    freeMatrix(C, A_row, B_col);
+    return 0;
+}
